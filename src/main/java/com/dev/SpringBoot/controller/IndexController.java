71d241bccc897a960c66e413b474b5e39abd3029
lovely9571 <lovaly9571@naver.com>
2019-04-25 14:18:38 +0900

    최초 프로젝트 생성

--- src/main/java/com/dev/SpringBoot/controller/IndexController.java
+++ src/main/java/com/dev/SpringBoot/controller/IndexController.java
@@ -8,15 +8,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-//@RequestMapping("/")
+@RequestMapping("/")
 public class IndexController {
-    @GetMapping("/hello")
+    @GetMapping("")
     public Map<String, Integer> rootGET() {
         Map<String, Integer> result = new HashMap<>();
         result.put("a", 1);
         result.put("b", 2);
         result.put("c", 3);
         result.put("d", 4);
+        result.put("e", 5);
+        result.put("f", 6);
         return result;
     }
 }
